e83c17aa18e20603412d3f66cb868356a7a1820e
Kiran GK <Kiran.Gopalakrishnan@ust.com>
2022-10-27 22:59:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterUserComponent } from './user/register-user/register-user.component';
+import { HomeUserComponent } from './user/home-user/home-user.component';
+import { BooksComponent } from './user/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterUserComponent,
+    HomeUserComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
